ab6d55a88e9431be9e3abb99390d46279bf274ab
Adam Grącikowski <01180781@pw.edu.pl>
2025-04-16 08:56:47 +0200

    fix issue with return

--- Lab02/morse/src/morse.c
+++ Lab02/morse/src/morse.c
@@ -37,7 +37,7 @@ void clear_sequence(MorseNode **head, MorseNode **tail)
         free(current);
         current = next;
     }
-    
+
     *head = NULL;
     *tail = NULL;
 }
@@ -106,7 +106,8 @@ void display_morse_sequence(gpio_t *diode, MorseNode *iterator)
 /* Buttons */
 void close_free_button(gpio_t **button)
 {
-    if(gpio_close(*button) < 0){
+    if (gpio_close(*button) < 0)
+    {
         fprintf(stderr, "gpio_close(): %s\n", gpio_errmsg(*button));
         gpio_free(*button);
         exit(EXIT_FAILURE);
@@ -116,36 +117,44 @@ void close_free_button(gpio_t **button)
 }
 void open_button(gpio_t **button, int pin, const char *path)
 {
-    if((*button = gpio_new()) == NULL){ 
+    if ((*button = gpio_new()) == NULL)
+    {
         fprintf(stderr, "gpio_new: Failed to allocate a GPIO handle.\n");
         exit(EXIT_FAILURE);
     }
 
-    if(gpio_open(*button, path, pin, GPIO_DIR_IN) < 0){
+    if (gpio_open(*button, path, pin, GPIO_DIR_IN) < 0)
+    {
         fprintf(stderr, "gpio_open(): %s\n", gpio_errmsg(*button));
         gpio_free(*button);
         exit(EXIT_FAILURE);
     }
 
-    if(gpio_set_edge(*button, GPIO_EDGE_FALLING) < 0){
+    if (gpio_set_edge(*button, GPIO_EDGE_FALLING) < 0)
+    {
         fprintf(stderr, "gpio_set_edge(): %s\n", gpio_errmsg(*button));
         close_free_button(button);
         exit(EXIT_FAILURE);
     }
 }
-bool has_miliseconds_passed(uint64_t start, uint64_t end, uint64_t miliseconds){
+bool has_miliseconds_passed(uint64_t start, uint64_t end, uint64_t miliseconds)
+{
     return (end - start) > (miliseconds * 1000000ULL);
 }
-bool empty_queue(gpio_t **button, gpio_edge_t *edge, uint64_t *button_timestamp) {
+bool empty_queue(gpio_t **button, gpio_edge_t *edge, uint64_t *button_timestamp)
+{
     uint64_t timestamp = 0;
 
-    while(gpio_poll(*button, DEBOUNCE_DELAY_MILISECONDS) > 0){
-        if (gpio_read_event(*button, edge, &timestamp) < 0) {
+    while (gpio_poll(*button, DEBOUNCE_DELAY_MILISECONDS) > 0)
+    {
+        if (gpio_read_event(*button, edge, &timestamp) < 0)
+        {
             fprintf(stderr, "gpio_read_event(): %s\n", gpio_errmsg(*button));
             return false;
         }
         *button_timestamp = timestamp;
         fprintf(stderr, "Bounce...\n");
-        return true;
     }
+
+    return true;
 }
